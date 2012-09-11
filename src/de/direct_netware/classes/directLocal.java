90d9e89a8639cb71e187222dddb128263f92c469
@4u <web@direct-netware.de>
2012-09-11 15:52:49 +0200

    Relicensed source code under the conditions of the Mozilla Public License, v. 2.0

--- src/de/direct_netware/classes/directLocal.java
+++ src/de/direct_netware/classes/directLocal.java
@@ -9,11 +9,11 @@ Java Application Services
 (C) direct Netware Group - All rights reserved
 http://www.direct-netware.de/redirect.php?jas
 
-This work is distributed under the W3C (R) Software License, but without any
-warranty; without even the implied warranty of merchantability or fitness
-for a particular purpose.
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------------------------
-http://www.direct-netware.de/redirect.php?licenses;w3c
+http://www.direct-netware.de/redirect.php?licenses;mpl2
 ----------------------------------------------------------------------------
 NOTE_END //n*/
 /**
@@ -29,8 +29,8 @@ NOTE_END //n*/
 * @copyright (C) direct Netware Group - All rights reserved
 * @package   jas_core
 * @since     v0.1.00
-* @license   http://www.direct-netware.de/redirect.php?licenses;w3c
-*            W3C (R) Software License
+* @license   http://www.direct-netware.de/redirect.php?licenses;mpl2
+*            Mozilla Public License, v. 2.0
 */
 
 package de.direct_netware.classes;
